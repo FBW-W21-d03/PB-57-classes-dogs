dccd079514671e5b522430e4111cc68e67d9e39f
ShiinyFlakes <mago_elm@hotmail.com>
2022-01-17 12:01:30 +0100

    Lösung

--- script.js
+++ script.js
@@ -4,10 +4,13 @@ class hund {
       this.rasse = rasse;
       this.alter = alter;
     }
-  };
-  let hund1 = new hund ("Name","rasse", "alter" );
-  let hund2 = new hund ("","","");
-  let hund3 = new hund ("","","");
+    bellen(){
+        console.log("Wuff! Wuff! Wuff!");
+    }
+}
+  let hund1 = new hund ("Bello","Husky", "4" );
+  let hund2 = new hund ("Bruno","Terrier","3");
+  let hund3 = new hund ("Bratan","Pitbull","2");
 
   const hunde = [hund1, hund2, hund3];
   console.log(hunde);
