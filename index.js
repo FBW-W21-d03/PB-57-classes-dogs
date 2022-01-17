39686d48908460e43a6a69289c358d217c470516
sprotte <metatron1972@live.de>
2022-01-17 12:16:30 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 "use strict";
 
-class Klasse {
+class Hunde {
   constructor(Hundename, Rasse, Alter) {
-    this.Hundename = Hundename;
-    this.Rasse = Rasse;
-    this.Alter = Alter;
+    this.hundeName = Hundename;
+    this.hundeRasse = Rasse;
+    this.hundeAlter = Alter;
+  }
+  HundBellen() {
+    console.log("Bell! Bell! Fass!");
   }
 }
+// 2 )
+let hund_1 = new Hunde("Asgard", "Rottweiler", 9);
+let hund_2 = new Hunde("Trixi", "Dakel", 12);
+let hund_3 = new Hunde("Rex", "Schaeferhund", 4);
+// 3 )
+const alleHunde = [];
+let hunde = alleHunde.push(hund_1, hund_2, hund_3);
+console.log(alleHunde);
+hund_1.HundBellen();
+hund_2.HundBellen();
+hund_3.HundBellen();
